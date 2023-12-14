44ff0cd09c06e44a9d5837677e93ae28a7162305
matwell <marcus.atwell@gmail.com>
2023-12-14 00:48:06 +0000

    Stopping day 5 part 2

--- src/days/5/Puzzle.ts
+++ src/days/5/Puzzle.ts
@@ -53,39 +53,44 @@ const second = (input: string) => {
     .map((s) => parseInt(s));
 
   const seeds = [];
+  const minLocations: number[] = [];
 
   for (let i = 0; i < seedRanges.length; i = i + 2) {
-    seeds.push(...generateConsecutiveNumbers(seedRanges[i], seedRanges[i + 1]));
+    const seeds = generateConsecutiveNumbers(seedRanges[i], seedRanges[i + 1]);
+
+    const locations = seeds.map((seed) => {
+      const soil = sourceToDestination(almanac, seed, 'seed-to-soil');
+      const fertilizer = sourceToDestination(
+        almanac,
+        soil,
+        'soil-to-fertilizer'
+      );
+      const water = sourceToDestination(
+        almanac,
+        fertilizer,
+        'fertilizer-to-water'
+      );
+      const light = sourceToDestination(almanac, water, 'water-to-light');
+      const temperature = sourceToDestination(
+        almanac,
+        light,
+        'light-to-temperature'
+      );
+      const humidity = sourceToDestination(
+        almanac,
+        temperature,
+        'temperature-to-humidity'
+      );
+      const location = sourceToDestination(
+        almanac,
+        humidity,
+        'humidity-to-location'
+      );
+      return location;
+    });
+    minLocations.push(Math.min(...locations));
   }
-
-  const locations = seeds.map((seed) => {
-    const soil = sourceToDestination(almanac, seed, 'seed-to-soil');
-    const fertilizer = sourceToDestination(almanac, soil, 'soil-to-fertilizer');
-    const water = sourceToDestination(
-      almanac,
-      fertilizer,
-      'fertilizer-to-water'
-    );
-    const light = sourceToDestination(almanac, water, 'water-to-light');
-    const temperature = sourceToDestination(
-      almanac,
-      light,
-      'light-to-temperature'
-    );
-    const humidity = sourceToDestination(
-      almanac,
-      temperature,
-      'temperature-to-humidity'
-    );
-    const location = sourceToDestination(
-      almanac,
-      humidity,
-      'humidity-to-location'
-    );
-    return location;
-  });
-
-  return Math.min(...locations);
+  return Math.min(...minLocations);
 };
 
 const expectedSecondSolution = 'solution 2';
